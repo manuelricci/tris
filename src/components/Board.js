3a6e441ed71cff2bf42ddfa6be020493619ff6fd
Manuel Ricci <manuel@webtea.it>
2023-08-10 19:00:39 +0200

    Aggiunto componente Board

---
+++ src/components/Board.js
@@ -0,0 +1,11 @@
+import Square from './Square';
+
+const Board = ( { squares, onClick } ) => (
+	<div className="board">
+		{ squares.map( ( square, i ) => (
+			<Square key={ i } value={ square } onClick={ () => onClick( i ) } />
+		) ) }
+	</div>
+);
+
+export default Board;
